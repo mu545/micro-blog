c746438c039a46b942fd5317acb487bfda1f8c8d
musa sutisna <mus4.sutisn4@gmail.com>
2019-07-18 15:04:22 +0700

    Remove not used resources
    * Remove all not used resource files from public directory
    * Remove codemirror from resources
    
    Fixes #13

--- public/stisla/js/scripts.js
+++ public/stisla/js/scripts.js
@@ -352,18 +352,6 @@ $(function() {
     });
   }
 
-  if(window.CodeMirror) {
-    $(".codeeditor").each(function() {
-      let editor = CodeMirror.fromTextArea(this, {
-        lineNumbers: true,
-        theme: "duotone-dark",
-        mode: 'javascript',
-        height: 200
-      });
-      editor.setSize("100%", 200);
-    });
-  }
-
   // Follow function
   $('.follow-btn, .following-btn').each(function() {
     var me = $(this),
